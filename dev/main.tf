9c58f37d6779c77500040c7360e0379f349a003d
David Charbonnier <david.charbonnier@objectif-libre.com>
2022-05-15 22:56:47 -0400

    adding missing vars and moving fixed values as new vars

--- dev/main.tf
+++ dev/main.tf
@@ -1,14 +1,14 @@
 module "cluster" {
   source                   = "../../cloud-foundation-fabric/modules/gke-cluster"
   project_id               = var.project_id
-  name                     = var.cluster_name
+  name                     = var.gke_cluster_name
   location                 = var.location
   network                  = var.vpc_self_links.dev-spoke-0
-  subnetwork               = "dev-infra-hosting-0-nane1"
-  secondary_range_pods     = "gke-pods"
-  secondary_range_services = "gke-services"
+  subnetwork               = var.gke_subnetwork
+  secondary_range_pods     = var.gke_secondary_range_pods
+  secondary_range_services = var.gke_secondary_range_services
   # addons = null
-  authenticator_security_group = "gcp-team-infra@davidcharbonnier.fr"
+  authenticator_security_group = var.gke_authenticator_security_group
   # cluster_autoscaler = {
   #   enabled    = false
   #   cpu_min    = 0
@@ -46,7 +46,7 @@ module "cluster" {
     master_ipv4_cidr_block  = "192.168.0.0/28"
     master_global_access    = false
   }
-  release_channel = "rapid"
+  release_channel = var.gke_release_channel
 }
 
 module "nodepool" {
@@ -55,15 +55,13 @@ module "nodepool" {
   cluster_name = module.cluster.name
   location     = var.location
   # autoscaling_config = null
-  initial_node_count = 2
+  initial_node_count = var.gke_nodepool_initial_node_count
   management_config = {
     auto_repair  = true
     auto_upgrade = true
   }
-  node_preemptible = true
-
-
-  node_disk_size    = 30
-  node_image_type   = "cos_containerd"
-  node_machine_type = "e2-small"
+  node_preemptible = var.gke_nodepool_node_preemptible
+  node_disk_size    = var.gke_nodepool_node_disk_size
+  node_image_type   = var.gke_nodepool_node_image_type
+  node_machine_type = var.gke_nodepool_node_machine_type
 }
--- dev/variables.tf
+++ dev/variables.tf
@@ -42,3 +42,65 @@ variable "project_id" {
   description = "Project id."
   type        = string
 }
+
+variable "location" {
+  description = "Region where to create resources."
+  type = string
+}
+
+variable "gke_cluster_name" {
+  description = "Name of GKE cluster."
+  type = string
+}
+
+variable "gke_subnetwork" {
+  description = "Name of GKE subnetwork."
+  type = string
+}
+
+variable "gke_secondary_range_pods" {
+  description = "Name of GKE secondary IP range for pods."
+  type = string
+}
+
+variable "gke_secondary_range_services" {
+  description = "Name of GKE secondary IP range for services."
+  type = string
+}
+
+variable "gke_authenticator_security_group" {
+  description = "Name of GKE group for authentication."
+  type = string
+}
+
+variable "gke_release_channel" {
+  description = "Name of GKE release channel."
+  type = string
+}
+
+variable "gke_nodepool_initial_node_count" {
+  description = "Initial number of nodes in GKE nodepool."
+  type = number
+}
+
+variable "gke_nodepool_node_preemptible" {
+  description = "GKE nodepool preemptible nodes."
+  type = bool
+  default = true
+}
+
+variable "gke_nodepool_node_disk_size" {
+  description = "GKE nodepool node disk size."
+  type = number
+}
+
+variable "gke_nodepool_node_image_type" {
+  description = "GKE nodepool node image type."
+  type = string
+  default = "cos_containerd"
+}
+
+variable "gke_nodepool_node_machine_type" {
+  description = "GKE nodepool node machine type."
+  type = string
+}
