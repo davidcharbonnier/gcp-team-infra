973cac2ffccbbad7bf28ef97a08afca50f02d405
David Charbonnier <david.charbonnier@objectif-libre.com>
2022-06-17 07:47:22 -0400

    moving argocd values file

--- dev/main.tf
+++ dev/main.tf
@@ -210,6 +210,6 @@ resource "helm_release" "argocd" {
   namespace        = "argocd"
   create_namespace = true
   values = [
-    "${file("../../deployment-config/argocd/argocd.yaml")}"
+    "${file("../../deployment-config/configurations/argocd/values.yaml")}"
   ]
 }
