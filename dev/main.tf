07363801fa3d553039ac8f61c332ef7965ae14ee
David Charbonnier <david.charbonnier@objectif-libre.com>
2022-06-17 08:15:12 -0400

    removing nginx ingress deployment from terraform code

--- dev/main.tf
+++ dev/main.tf
@@ -116,17 +116,6 @@ module "nodepool" {
   ]
 }
 
-resource "helm_release" "ingress-nginx" {
-  name             = "ingress-nginx"
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  namespace        = "ingress-nginx"
-  create_namespace = true
-  values = [
-    "${file("values/ingress-nginx.yaml")}"
-  ]
-}
-
 resource "helm_release" "external-dns" {
   name             = "external-dns"
   repository       = "https://kubernetes-sigs.github.io/external-dns/"
