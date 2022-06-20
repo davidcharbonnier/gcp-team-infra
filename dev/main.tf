345464527c02defb6a05122794444fb8d6bd19c4
David Charbonnier <david.charbonnier@objectif-libre.com>
2022-06-19 23:31:59 -0400

    removing external-dns deployment

--- dev/main.tf
+++ dev/main.tf
@@ -116,34 +116,6 @@ module "nodepool" {
   ]
 }
 
-resource "helm_release" "external-dns" {
-  name             = "external-dns"
-  repository       = "https://kubernetes-sigs.github.io/external-dns/"
-  chart            = "external-dns"
-  namespace        = "external-dns"
-  create_namespace = true
-
-  set {
-    name  = "provider"
-    value = "google"
-  }
-
-  set {
-    name  = "triggerLoopOnEvent"
-    value = true
-  }
-
-  set {
-    name  = "domainFilters"
-    value = "{${join(",", var.external_dns_domains)}}"
-  }
-
-  set {
-    name  = "txtOwnerId"
-    value = "gke-${module.cluster.name}-${var.project_id}"
-  }
-}
-
 resource "helm_release" "crossplane" {
   name             = "crossplane"
   repository       = "https://charts.crossplane.io/stable"
